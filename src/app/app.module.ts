b72e2a9d170c4513ffa00923bbf1bd9c51e0a6b1
Mateus <mateushen19@outlook.com>
2022-04-07 20:26:32 -0300

    Criando os principais componentes e estilizando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotStartedComponent } from './components/not-started/not-started.component';
+import { InProgressComponent } from './components/in-progress/in-progress.component';
+import { CompletedComponent } from './components/completed/completed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotStartedComponent,
+    InProgressComponent,
+    CompletedComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/not-started/not-started.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-not-started',
+  templateUrl: './not-started.component.html',
+  styleUrls: ['./not-started.component.scss']
+})
+export class NotStartedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
